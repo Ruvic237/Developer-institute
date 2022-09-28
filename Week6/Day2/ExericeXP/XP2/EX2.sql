ac2a5ff37fd13a7743e26871c935cf4d6d8d72cb
Ruvic237 <bambavarnes@gmail.com>
2022-09-28 13:10:53 +0200

    update

---
+++ Week6/Day2/ExericeXP/XP2/EX2.sql
@@ -0,0 +1,14 @@
+-- SELECT * FROM customer;
+-- SELECT first_name, last_name FROM customer AS full_name;
+-- SELECT DISTINCT create_date FROM customer;
+-- SELECT * FROM customer ORDER BY first_name DESC;
+-- SELECT film_id,title,description,release_year,rental_rate FROM film ORDER BY rental_rate ASC;
+-- SELECT address,phone FROM address WHERE district ='Texas';
+-- SELECT * FROM film WHERE film_id = 15 OR film_id = 150;
+-- SELECT film_id,title,description,length,rental_rate FROM film WHERE title = 'Cider Desire';
+-- SELECT film_id,title,description,length,rental_rate FROM film WHERE title = 'Ci%';
+-- SELECT * FROM film ORDER BY rental_rate ASC LIMIT 10;
+-- SELECT * FROM film ORDER BY rental_rate ASC OFFSET 10 FETCH NEXT 10 ROWS ONLY;
+-- SELECT customer.first_name, amount,payment_date FROM payment INNER JOIN customer ON customer.customer_id = payment.customer_id;
+-- SELECT city,country FROM city INNER JOIN country ON country.country_id = city.country_id;
+
